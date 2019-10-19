dabad907ea25711c5e3d3058adde5dd8240efcee
Jonas Tratz <jonastratz@yahoo.de>
2019-10-19 18:35:14 +0200

    Update Aufgabe1.java

--- Aufgabe1.java
+++ Aufgabe1.java
@@ -14,5 +14,10 @@ public class Aufgabe1 {
             .count();
         System.out.println(count);
 
+        strings.stream()
+            .filter(low -> low.length() > 1)
+            .filter(low -> low.length() / 2 == 0);
+
+        System.out.println(strings);
     }
 }
